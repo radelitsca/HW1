d4d87806cb3876c7153485c03fced0a3bad0eb07
radelitsca <radelitsca@gmail.com>
2022-08-24 21:51:50 +0300

    hw-1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String text = "This is my first homework";
+        print(text);
+        printSum(5, 10);
+    }
+    private static void print(String s) {
+        System.out.println(s);
+    }
+
+    private static void printSum(int a, int b){
+        System.out.println(a+b);
+    }
+
+}
