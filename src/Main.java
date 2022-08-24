85039836f53debbfa9c6e092b8f5b7e6c6394f21
radelitsca <radelitsca@gmail.com>
2022-08-24 21:56:07 +0300

    add hw-1 file

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         String text = "This is my first homework";
-        print(text);
+        printMessage(text);
         printSum(5, 10);
     }
-    private static void print(String s) {
+
+    private static void printMessage(String s) {
         System.out.println(s);
     }
 
     private static void printSum(int a, int b){
         System.out.println(a+b);
     }
-
 }
